40a8f3ccb620421d397919385eeaa93c8785b700
BigNordman <sergei.vershinin@gmail.com>
2016-11-26 14:42:18 +0500

    Initial commit

---
+++ common/src/main/java/com/nordman/big/smsparkinglib/ParkZone.java
@@ -0,0 +1,40 @@
+package com.nordman.big.smsparkinglib;
+
+/**
+ * Created by Sergey on 26.11.2016.
+ *
+ */
+
+import com.vividsolutions.jts.geom.Polygon;
+
+/**
+ * Created by s_vershinin on 11.01.2016.
+ *
+ */
+public class ParkZone {
+    private Integer zoneNumber;
+    private String zoneDesc;
+    private Polygon zonePolygon;
+
+    public ParkZone(Polygon zonePolygon, Integer zoneNumber, String zoneDesc) {
+        this.zonePolygon = zonePolygon;
+        this.zoneNumber = zoneNumber;
+        this.zoneDesc = zoneDesc;
+    }
+
+    public Polygon getZonePolygon() {
+        return zonePolygon;
+    }
+
+    public String getZoneDesc() {
+
+        return zoneDesc;
+    }
+
+
+    public Integer getZoneNumber() {
+
+        return zoneNumber;
+    }
+
+}
